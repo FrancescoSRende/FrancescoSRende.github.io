fe3af03817f3dbd3a4a0927d175577c6961773e7
FrancescoSRende <francesco.rende@ucc.on.ca>
2021-01-20 15:04:20 -0500

    fixed convert to meters program, commented for learning

--- Python Contest Questions/convertToMeters.py
+++ Python Contest Questions/convertToMeters.py
@@ -1,12 +1,12 @@
 def convertToMeters(a,b):
 
     if (b == "m"):
-        print(str(a) + " " + b)
+        print(str(a))
     elif (b == "dm"):
-        print(str(a / 10) + " m")
+        print(str(a / 10))
     elif (b == "cm"):
-        print(str(a / 100) + " m")
+        print(str(a / 100))
     elif (b == "mm"):
-        print(str(a / 1000) + " m")
+        print(str(a / 1000))
 
 convertToMeters(39485,'mm')
