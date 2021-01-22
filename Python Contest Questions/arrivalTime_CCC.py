9a9130d0df22d003c413ebdfafc6749eb25851f6
FrancescoSRende <francesco.rende@ucc.on.ca>
2021-01-22 10:05:03 -0500

    these problems are obscenely difficult

--- Python Contest Questions/arrivalTime_CCC.py
+++ Python Contest Questions/arrivalTime_CCC.py
@@ -1,19 +1,16 @@
 time = input()
 
 minutes = int(time[0:2]) * 60 + int(time[3:5])
-arrival = 0
+arrival = 00:00
 minutesArrival = 0
 
-if minutes <= 300 or minutes >= 600 and minutes <= 780 or minutes >= 1140:
-    arrival = str(int(time[0:2]+2)) + time[2:5]
-    if int(arrival[0:2]) >= 24:
-        print(str(int(arrival[0:2] - 24)) + time[2:5])
-    else:
-        print(arrival)
-
-if (420 - minutes) > 0:
-    timeSlow = 420 - minutes
-    timeFast = 120 - timeSlow
-    minutesArrival = minutes + timeFast + timeSlow * 2
+if minutes <= 300 or minutes >= 1140 or minutes >= 600 and minutes <= 780:
+    arrival = str(int(time[0:2]) + 2) + time[2:5]
+    print(arrival)
+elif minutes >= 420 and minutes < 480 or minutes >= 900 and minutes < 1020:
+    arrival = str(int(time[0:2]) + 4) + time[2:5]
+    print(arrival)
+elif minutes > 300 and < 420:
+    slowMin = 420 - minutes
+    minutesArrival = minutes + (120 - slowMin) + slowMin * 2
     arrival = str()
-    print()
