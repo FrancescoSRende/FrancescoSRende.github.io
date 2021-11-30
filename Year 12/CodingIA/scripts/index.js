01c788692797e1372982766c90f7334e1987c1ec
FrancescoSRende <francesco.rende@ucc.on.ca>
2021-11-29 21:04:10 -0500

    added button for admins to view suggestions... still a long way to go

--- Year 12/CodingIA/scripts/index.js
+++ Year 12/CodingIA/scripts/index.js
@@ -7,14 +7,60 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-options = {}
-var elems = document.querySelectorAll('.sidenav');
-var instances = M.Sidenav.init(elems, options);
+    options = {}
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
 });
 
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
 // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// defining elements
+const logoutBTN = document.getElementById("logoutBTN")
+const newUserBTN = document.getElementById("newUserBTN")
+const loginBTN = document.getElementById("loginBTN")
+const songBTN = document.getElementById("songBTN")
+const adminSugBTN = document.getElementById("adminSugBTN")
+const createCardBTN = document.getElementById("createCardBTN")
+
+const loginForm = document.getElementById("loginForm")
+const signUpForm = document.getElementById("signUpForm")
+const songForm = document.getElementById("songForm")
+const createCardForm = document.getElementById("createCardForm")
+
+const cardDisplay = document.getElementById("cardDisplay")
+const sugDisplay = document.getElementById("sugDisplay")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -46,6 +92,11 @@ users = [
     {
         "email": "test",
         "password": "f",
+        "admin": true
+    },
+    {
+        "email": "bad",
+        "password": "a",
         "admin": false
     }
 ]
@@ -69,23 +120,27 @@ activeuser = null
 
 // <!-- fake login -->
 
-
-const login_form = document.getElementById("login-form")
-
-login_form.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", (e) => {
 
     e.preventDefault()
 
-    attemptEmail = login_form["user_email"].value
-    attemptPword = login_form["user_password"].value
+    attemptEmail = loginForm["user_email"].value
+    attemptPword = loginForm["user_password"].value
 
     for (let i = 0; i < users.length; i++) {
         if (users[i]["email"] == attemptEmail) {
             if (users[i]["password"] == attemptPword) {
                 // sidenavTrigger.style.display = "block"
-                logout_btn.style.display = "block"
+                logoutBTN.style.display = "block"
+                songBTN.style.display = "block"
+                cardDisplay.style.display = "block"
+
                 newUserBTN.style.display = "none"
                 loginBTN.style.display = "none"
+                if (users[i]["admin"] == true) {
+                    adminSugBTN.style.display = "block"
+                    createCardBTN.style.display = "block"
+                }
                 break
             }
         }
@@ -125,15 +180,12 @@ login_form.addEventListener("submit", (e) => {
 
 // <!-- fake sign up -->
 
-
-const sign_up_form = document.getElementById("signup-form")
-
-sign_up_form.addEventListener("submit", (e) => {
+signUpForm.addEventListener("submit", (e) => {
 
 e.preventDefault()
 
-newEmail = sign_up_form["new_email"].value
-newPword = sign_up_form["new_password"].value
+newEmail = signUpForm["new_email"].value
+newPword = signUpForm["new_password"].value
 
 var UCCcheck = false
 
@@ -177,7 +229,10 @@ if (UCCcheck === true) {
 
 
     // sidenavTrigger.style.display = "block"
-    logout_btn.style.display = "block"
+    logoutBTN.style.display = "block"
+    songBTN.style.display = "block"
+    cardDisplay.style.display = "block"
+
     newUserBTN.style.display = "none"
     loginBTN.style.display = "none"
 
@@ -209,17 +264,402 @@ if (UCCcheck === true) {
 
 
 // <!-- fake logout -->
-
-
-const logout_btn = document.getElementById("logout_btn")
-
-logout_btn.addEventListener("click", (e) => {
+logoutBTN.addEventListener("click", (e) => {
 
 
 // sidenavTrigger.style.display = "none"
-logout_btn.style.display = "none"
+logoutBTN.style.display = "none"
+songBTN.style.display = "none"
+cardDisplay.style.display = "none"
+adminSugBTN.style.display = "none"
+sugDisplay.style.display = "none"
+createCardBTN.style.display = "none"
+
 newUserBTN.style.display = "block"
 loginBTN.style.display = "block"
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// list of card info
+cardInfo = [
+    {"title": "Ape Dos Mil",
+    "band": "Glassjaw",
+    "genre": "Alternative",
+    "approved": true
+    },
+    {"title": "badsong",
+    "band": "a band",
+    "genre": "jazz",
+    "approved": false
+    },
+    {"title": "song",
+    "band": "band",
+    "genre": "genre",
+    "approved": true
+    },
+    {"title": "suggestion",
+    "band": "napalm death",
+    "genre": "swing",
+    "approved": false
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// generate cards
+
+//Works the same as previous case, but now a function 
+ 	//creates a card and returns it with the program can use. 
+ 	//This is nice because you can now use parameters to modify the
+ 	//card contents. 
+ 	//Note: There is a unique format for adding strings using `` 
+ 	//		notation
+ 	//Note: You can also use DOM to build the element
+ 	function createCard(title, band, genre) {
+        const card = `
+
+                <div class="container">
+                        <h2 class = "col s12 m12 l6 center-align">${genre}</h2>
+
+                        <div class="row">
+
+                            <div class="col s12 l6">
+                                <div class="card">
+
+                                    <div class="card-image">
+                                        <div class="video-container">
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/4b8ZfLKxoRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                        <!-- <a href="#" class="halfway-fab btn-floating pink pulse">
+                                            <i class="material-icons">favorite</i>
+                                        </a> -->
+                                    </div>
+                                    <div class="card-content">
+                                        <span class="card-title">${band} - ${title}</span>
+                                        <p>Ashes of the Wake, Sacrament, As the Palaces Burn, Lamb of God (self-titled)</p>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="https://en.wikipedia.org/wiki/Lamb_of_God_(band)">More Details</a>
+                                        <a href="https://www.youtube.com/watch?v=HL9kaJZw8iw&ab_channel=lambofgodVEVO">Laid to Rest</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+           `;
+
+           return card
+
+    }
+
+
+    const cd = document.getElementById("cardDisplay")
+
+
+    console.log(cd)
+   
+   //creates an extra div in the structure
+   //Suggestion: Remove the div for the col in the 
+   //const then embed in that.  It will require you set the
+   //class name. 
+//    newDiv = document.createElement("div")
+//    newDiv.innerHTML = createCard("AAAA","AA");
+//    cd.innerHTML = createCard("BBBB","BB") + createCard("CCCC","CC");
+//    cd.appendChild(newDiv)
+
+   //We can use a loop to add a large amount of 
+    for (i = 0; i < cardInfo.length; i = i + 1) {
+        if (cardInfo[i]["approved"] == true) {
+            newDiv = document.createElement("div")
+            newDiv.innerHTML = createCard(cardInfo[i]["title"],cardInfo[i]["band"],cardInfo[i]["genre"])
+            cd.appendChild(newDiv)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// numbers for suggestions
+sugNums = []
+
+
+
+// generate suggestion cards
+
+ 	function createSugCard(title, band, genre, number) {
+        const card = `
+
+                <div class="container">
+                        <h2 class = "col s12 m12 l6 center-align">${genre}</h2>
+
+                        <div class="row">
+
+                            <div class="col s12 l6">
+                                <div class="card">
+
+                                    <div class="card-image">
+                                        <div class="video-container">
+                                            <iframe width="560" height="315" src="https://www.youtube.com/embed/4b8ZfLKxoRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        </div>
+                                        <!-- <a href="#" class="halfway-fab btn-floating pink pulse">
+                                            <i class="material-icons">favorite</i>
+                                        </a> -->
+                                    </div>
+                                    <div class="card-content">
+                                        <span class="card-title">${band} - ${title}</span>
+                                        <p>Ashes of the Wake, Sacrament, As the Palaces Burn, Lamb of God (self-titled)</p>
+                                    </div>
+                                    <div class="card-action">
+                                        <div class = "row">
+                                            <a href="#" id="acceptBTN${number}" class="modal-trigger green btn">Accept Suggestion</a>
+                                        </div>
+                                        <div class = "row">
+                                            <a href="#" id="rejectBTN${number}" class="modal-trigger red darken-1 btn">Reject Suggestion</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+           `;
+
+           return card
+
+    }
+
+
+    const sd = document.getElementById("sugDisplay")
+
+
+    for (i = 0; i < cardInfo.length; i = i + 1) {
+        if (cardInfo[i]["approved"] == false) {
+            newSugDiv = document.createElement("div")
+            newSugDiv.innerHTML = createSugCard(cardInfo[i]["title"],cardInfo[i]["band"],cardInfo[i]["genre"],i.toString())
+            sd.appendChild(newSugDiv)
+            sugNums.push(i.toString())
+            // var numAcceptBTN = "acceptBTN" + i.toString()
+            // var numRejectBTN = "rejectBTN" + i.toString()
+            // const numAcceptBTN = document.getElementById("numAcceptBTN")
+        }
+    }
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+   // <!-- song submission - create card -->
+
+songForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    suggestedSong = songForm["song_title"].value
+    suggestedBand = songForm["band_name"].value
+    suggestedGenre = songForm["genre"].value
+
+    suggestion = {
+        "title": suggestedSong,
+        "band": suggestedBand,
+        "genre": suggestedGenre,
+        "approved": false
+    }
+
+    cardInfo.push(suggestion)
+
+    console.log(cardInfo)
+
+    // newDiv = document.createElement("div")
+    // newDiv.innerHTML = createCard(suggestedSong,suggestedBand,suggestedGenre)
+    // cd.appendChild(newDiv)
+
+    const modal = document.querySelector("#song-modal")
+    M.Modal.getInstance(modal).close();
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <!-- button to let admins view suggested songs -->
+adminSugBTN.addEventListener("click", (e) => {
+
+
+    sugDisplay.style.display = "block"
+    
+    
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <!-- accept suggestion button -->
+adminSugBTN.addEventListener("click", (e) => {
+
+
+    sugDisplay.style.display = "block"
+    
+    
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // <!-- song submission - admin create card -->
+    // HOPEFULLY TEMPORARY FIX
+
+    createCardForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    createSong = createCardForm["create_song_title"].value
+    createBand = createCardForm["create_band_name"].value
+    createGenre = createCardForm["create_genre"].value
+
+    creation = {
+        "title": createSong,
+        "band": createBand,
+        "genre": createGenre,
+        "approved": true
+    }
+
+    cardInfo.push(creation)
+
+    newDiv = document.createElement("div")
+    newDiv.innerHTML = createCard(createSong,createBand,createGenre)
+    cd.appendChild(newDiv)
+
+    // newDiv = document.createElement("div")
+    // newDiv.innerHTML = createCard(suggestedSong,suggestedBand,suggestedGenre)
+    // cd.appendChild(newDiv)
+
+    const modal = document.querySelector("#create-card-modal")
+    M.Modal.getInstance(modal).close();
 
 
 
